92724d9211428a77bdbfd47769d06339da0e7bb2
ryanmpdev <ryanmaciel697@gmail.com>
2022-07-26 19:27:53 -0300

    Pré estilização

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component{
+
+  state = {
+    num1: '',
+    num2: '',
+    resultado: ''
+  }
+
+  divisao = () => {
+    this.setState({
+      resultado: this.state.num1 / this.state.num2
+    })
+  }
+
+  subtracao = () => {
+    this.setState({
+      resultado: this.state.num1 - this.state.num2
+    })
+  }
+
+  multiplicacao = () => {
+    this.setState({
+      resultado: this.state.num1 * this.state.num2
+    })
+  }
+
+  adicao = () => {
+    this.setState({
+      resultado: parseInt(this.state.num1) + parseInt(this.state.num2)
+    })
+  }
+
+  limpar = () => {
+    this.setState({
+      resultado: ""
+    })
+  }
+
+handleChange1 = (event) => {
+  this.setState({
+    num1:event.target.value
+  })
+ }
+
+ 
+ handleChange2 = (event) => {
+  this.setState({
+    num2: event.target.value
+  })
 }
 
-export default App;
+render(){
+  return(
+    <calculadora>
+      <title>Calculadora</title>
+           <input type="number" onChange={this.handleChange1} />
+           <input type="number" onChange={this.handleChange2}/>
+           <butoes>
+             <button onClick={this.adicao}>+</button>
+             <button onClick={this.subtracao}>-</button>
+             <button onClick={this.divisao}>/</button>
+             <button onClick={this.multiplicacao}>*</button>
+             <button onClick={this.limpar}>c</button>
+           </butoes>
+           <h2>{this.state.resultado}</h2>
+         </calculadora>
+  )
+}
+}
